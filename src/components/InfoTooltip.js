47aa906474c3604f3476bc1e858666655533e3fa
Mark Politiko <politikomark@yandex.ru>
2022-12-14 21:40:44 +0300

    Second commit for 12th project check (CSS, burger-menu, cards after logged in-check and etc.)

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -3,30 +3,32 @@ import rejectImg from "../images/LoginReject.svg";
 import successImg from "../images/LoginSuccess.svg";
 
 function InfoTooltip({ isOpen, onClose, isSuccessReg }) {
-  <div className={isOpen ? "popup popup_show" : "popup"}>
-    <div className="popup__content popup__tooltip">
-      <button
-        className="popup__close-button"
-        type="button"
-        aria-label="Закрыть"
-        onClick={onClose}
-      ></button>
-      <img
-        src={isSuccessReg ? successImg : rejectImg}
-        alt={
-          isSuccessReg
+  return (
+    <div className={isOpen ? "popup popup_show" : "popup"}>
+      <div className="popup__content popup__tooltip">
+        <button
+          className="popup__close-button"
+          type="button"
+          aria-label="Закрыть"
+          onClick={onClose}
+        ></button>
+        <img
+          src={isSuccessReg ? successImg : rejectImg}
+          alt={
+            isSuccessReg
+              ? "Вы успешно зарегистрировались!"
+              : "Что-то пошло не так. Попробуйте еще раз!"
+          }
+          className="popup__tooltip_image"
+        />
+        <p className="popup__tooltip_info">
+          {isSuccessReg
             ? "Вы успешно зарегистрировались!"
-            : "Что-то пошло не так. Попробуйте еще раз!"
-        }
-        className="popup__tooltip_image"
-      />
-      <p className="popup__tooltip_info">
-        {isSuccessReg
-          ? "Вы успешно зарегистрировались!"
-          : "Что-то пошло не так. Попробуйте еще раз!"}
-      </p>
+            : "Что-то пошло не так. Попробуйте еще раз!"}
+        </p>
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default InfoTooltip;
